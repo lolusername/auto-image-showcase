cace4f94a96e53d6ac2983a0967b37edd9710868
lolusername <to.atilio@gmail.com>
2024-02-04 01:02:37 -0500

    v1

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,85 @@
 <script>
+  import { onMount } from "svelte";
   import images from "./assets/photos.json";
   import ResponsiveImage from "./components/ResponsiveImage.svelte";
+  import { shuffleArray } from "./utils";
+  let mainWidth, mainHeight;
+
+  function setMainWidth() {
+    const windowAspectRatio = window.innerHeight / window.innerWidth;
+    if (windowAspectRatio > 1) {
+      return "100svw";
+    }
+    return `${Math.max(
+      10,
+      100 - (windowAspectRatio / featuredImage.aspectRatio) * 100
+    )}%`;
+  }
+
+  function setMainHeight() {
+    const windowAspectRatio = window.innerHeight / window.innerWidth;
+    if (windowAspectRatio < 1) {
+      return "100svh";
+    }
+    return `${Math.max(
+      10,
+      100 - (featuredImage.aspectRatio / windowAspectRatio) * 100
+    )}svh`;
+  }
+
+  let featuredImage = images[1];
+
+  function setFeaturedImage(image) {
+    featuredImage = image.detail;
+    mainHeight = setMainHeight();
+    mainWidth = setMainWidth();
+    scrollToNextImage(image);
+  }
+  function scrollToNextImage(image) {
+    const img = shuffledImages.find((shuffledImage) => {
+      return shuffledImage.fileName === image.detail.fileName;
+    });
+    const idx =
+      shuffledImages.indexOf(img) === shuffledImages.length - 1
+        ? 0
+        : shuffledImages.indexOf(img);
+
+    const nextImage = shuffledImages[idx + 1];
+    const nextImageElement = document.getElementById(nextImage.fileName);
+    setTimeout(() => {
+      nextImageElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "center",
+      });
+    }, 300);
+  }
+  const handleResize = () => {
+    mainHeight = setMainHeight();
+    mainWidth = setMainWidth();
+  };
+  onMount(() => {
+    mainHeight = setMainHeight();
+    mainWidth = setMainWidth();
+  });
+  $: shuffledImages = shuffleArray(images);
+  window.addEventListener("resize", handleResize);
+  window.addEventListener("orientationchange", handleResize);
 </script>
 
-{#each images as image}
-  <ResponsiveImage {image} />
-{/each}
-
-<style>
-</style>
+<div
+  class="bg-fixed bg-contain bg-bottom sm:bg-left-top bg-no-repeat min-h-svh"
+  style="background-image:url({featuredImage.fileName}-2400.jpeg)"
+>
+  <main
+    style="width: {mainWidth};height:{mainHeight}"
+    class="{mainWidth < 25
+      ? 'bg-stone-100 grid-cols-2 sm:grid-cols-1 md:gap-y-24 '
+      : 'md:gap-y-12 sm:gap-x-12 p-0'} overflow-auto md:border-l-4 border-stone-900 transition-all duration-300 justify-items-center
+      grid grid-cols-3 sm:grid-cols-1 p-4 gap-4 ml-auto ease-linear bg-stone-100 sticky bottom-0 sm:bottom-auto sm:relative"
+  >
+    {#each shuffledImages as image}
+      <ResponsiveImage {image} on:setFeaturedImage={setFeaturedImage} />
+    {/each}
+  </main>
+</div>
--- src/components/ResponsiveImage.svelte
+++ src/components/ResponsiveImage.svelte
@@ -1,19 +1,28 @@
 <script>
+  import { createEventDispatcher } from "svelte";
+
   export let image = {};
+
+  const dispatch = createEventDispatcher();
+
+  function setFeaturedImage(image) {
+    dispatch("setFeaturedImage", image);
+  }
 </script>
 
-<img
-  srcset={`${image.fileName}-480.webp 480w, ${image.fileName}-1200.webp 1200w, ${image.fileName}-2400.webp 2400w`}
-  sizes="(max-width: 600px),
+<section class="grid grid-cols-1 content-center col-span-1">
+  <button on:click={() => setFeaturedImage(image)}>
+    <img
+      id={image.fileName}
+      srcset={`${image.fileName}-480.webp 480w, ${image.fileName}-1200.webp 1200w, ${image.fileName}-2400.webp 2400w`}
+      sizes="(max-width: 600px),
          (max-width: 1200px),
          2400px"
-  src={`${image.fileName}-2400.jpeg`}
-  alt="Description"
-/>
-
-<style>
-  img {
-    width: 100%;
-    height: auto;
-  }
-</style>
+      src={`${image.fileName}-2400.jpeg`}
+      alt="Description"
+      class="{image.aspectRatio > 1
+        ? 'max-h-svh md:py-4'
+        : ''}  object-contain mx-auto"
+    />
+  </button>
+</section>
